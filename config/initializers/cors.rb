5590148bc72d7cfa164a2903742abd4db89174b6
Kimber Ogden <mx.kimber@gmail.com>
2023-06-03 14:59:49 -0500

    Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173'
-
-    resource '/plants.json',
-      headers: :any,
-      methods: [:get]
+    origins "localhost:3000", "localhost:5173"
+    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
