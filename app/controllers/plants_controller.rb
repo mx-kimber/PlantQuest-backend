f62355bddc82586b149edf2b9cbaa6c6f4386d9b
Kimber Ogden <mx.kimber@gmail.com>
2023-06-03 20:58:21 -0500

    Plants Show and Update actions

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -15,4 +15,22 @@ class PlantsController < ApplicationController
     )
     render :show
   end
+
+  def show
+    @plant = Plant.find_by(id: params[:id])
+    render :show
+  end
+
+  def update
+    @plant = Plant.find_by(id: params[:id])
+    @plant.update(
+      name: params[:name] || @plant.name,
+      description: params[:description] || @plant.description,
+      sun_amount: params[:sun_amount] || @plant.sun_amount,
+      days_to_water: params[:days_to_water] || @plant.days_to_water,
+    )
+   
+    render :show
+  end
+
 end
