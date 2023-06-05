e9af71655b2625d9f653ed4ace9e8e8889defde7
Kimber Ogden <mx.kimber@gmail.com>
2023-06-05 10:59:33 -0500

    Schedules Update and Destroy actions

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -21,4 +21,35 @@ class SchedulesController < ApplicationController
       render json: {errors: @schedule.errors.full_messages}, status: :unprocessable_entity
     end
   end
+
+  def update
+    @schedule = Schedule.find_by(id: params[:id])
+    if @schedule.update(
+      user_id: params[:user_id] || @schedule.user_id,
+      collected_plant_id: params[:collected_plant_id] || @schedule.collected_plant_id,
+      watering_start_date: params[:watering_start_date] || @schedule.watering_start_date,
+    )
+      render :show
+    else
+      render json: { errors: @schedule.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @schedule = Schedule.find_by(id: params[:id])
+    if confirm_destroy?
+      @schedule.destroy
+      render json: { message: "schedule destroyed successfully" }
+    else
+      render json: { message: "Deletion canceled" }
+    end
+  end
+  
+  private
+  
+  def confirm_destroy?
+    confirm_message = "Are you sure you want to delete this schedule?"
+    confirmation = params[:confirm]
+    confirmation == "true"
+  end
 end
