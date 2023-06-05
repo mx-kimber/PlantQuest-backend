099280cecb39a3fe112ba0c79931cd4dcf824601
Kimber Ogden <mx.kimber@gmail.com>
2023-06-04 22:14:45 -0500

    Add CollectedPlants create with validations

--- app/controllers/collected_plants_controller.rb
+++ app/controllers/collected_plants_controller.rb
@@ -5,6 +5,23 @@ class CollectedPlantsController < ApplicationController
     render :index
   end
 
+  def create
+    @collected_plant = CollectedPlant.new(
+      user_id: params[:user_id],
+      plant_id: params[:plant_id],
+      custom_name: params[:custom_name],
+      notes: params[:notes],
+      users_image: params[:users_image].presence || Plant.find(params[:plant_id]).users_image
+    )
+  
+    if @collected_plant.save
+      render :show
+    else
+      render json: { errors: @collected_plant.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+  
+
   def show
     @collected_plant = CollectedPlant.find(params[:id])
     render :show
--- app/models/collected_plant.rb
+++ app/models/collected_plant.rb
@@ -2,5 +2,11 @@ class CollectedPlant < ApplicationRecord
   belongs_to :user
   belongs_to :plant
   has_one :schedule
+
+  validates :user_id, presence: true
+  validates :plant_id, presence: true
+  validates :custom_name, presence: true
+  validates :notes, presence: true
+  validates :users_image, presence: true
 end
   
