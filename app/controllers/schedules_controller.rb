f540f07650bb7217fd98ae7ac2359f23ccfba975
Kimber Ogden <mx.kimber@gmail.com>
2023-06-05 10:07:13 -0500

    fix Schedules Create for basic implementation

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -7,23 +7,18 @@ class SchedulesController < ApplicationController
   def show
     @schedule = Schedule.find(params[:id])
     render :show
-  end
- 
+  end 
+
   def create
-    @user = current_user
-    @collected_plant = @user.collected_plants.find(params[:collected_plant_id])
-    @schedule = @collected_plant.build_schedule(schedule_params)
-
+    @schedule = Schedule.create(
+      user_id: params[:user_id],
+      collected_plant_id: params[:collected_plant_id],
+      watering_start_date: params[:watering_start_date],
+    )
     if @schedule.save
-      render json: @schedule, status: :created
+      render :show
     else
-      render json: { errors: @schedule.errors.full_messages }, status: :unprocessable_entity
+      render json: {errors: @schedule.errors.full_messages}, status: :unprocessable_entity
     end
-  end
-
-  private
-
-  def schedule_params
-    params.require(:schedule).permit(:watering_start_date)
   end
 end
