4b2fae8f5d9af4e7bb8217cbb40f6c0fdae5346e
Kimber Ogden <mx.kimber@gmail.com>
2023-06-06 23:27:06 -0500

    Remove/comment out unnecessary actions in Plants, remove/comment out validations for schedules

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -7,57 +7,57 @@ class PlantsController < ApplicationController
     render :index
   end
  
-  def create
-    @plant = Plant.create(
-      name: params[:name],
-      description: params[:description],
-      sun_amount: params[:sun_amount],
-      days_to_water: params[:days_to_water],
-    )
-    if @plant.save
-      render :show
-    else
-      render json: {errors: @plant.errors.full_messages}, status: :unprocessable_entity
-    end
-  end
+  # def create
+  #   @plant = Plant.create(
+  #     name: params[:name],
+  #     description: params[:description],
+  #     sun_amount: params[:sun_amount],
+  #     days_to_water: params[:days_to_water],
+  #   )
+  #   if @plant.save
+  #     render :show
+  #   else
+  #     render json: {errors: @plant.errors.full_messages}, status: :unprocessable_entity
+  #   end
+  # end
 
   def show
     @plant = Plant.find_by(id: params[:id])
     render :show
   end
 
-  def update
-    @plant = Plant.find_by(id: params[:id])
-    if @plant.update(
-      name: params[:name] || @plant.name,
-      description: params[:description] || @plant.description,
-      sun_amount: params[:sun_amount] || @plant.sun_amount,
-      days_to_water: params[:days_to_water] || @plant.days_to_water,
-    )
-      render :show
-    else
-      render json: { errors: @plant.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   @plant = Plant.find_by(id: params[:id])
+  #   if @plant.update(
+  #     name: params[:name] || @plant.name,
+  #     description: params[:description] || @plant.description,
+  #     sun_amount: params[:sun_amount] || @plant.sun_amount,
+  #     days_to_water: params[:days_to_water] || @plant.days_to_water,
+  #   )
+  #     render :show
+  #   else
+  #     render json: { errors: @plant.errors.full_messages }, status: :unprocessable_entity
+  #   end
+  # end
   
 
-  def destroy
-    @plant = Plant.find_by(id: params[:id])
-    if confirm_destroy?
-      @plant.destroy
-      render json: { message: "Plant destroyed successfully" }
-    else
-      render json: { message: "Deletion canceled" }
-    end
-  end
+  # def destroy
+  #   @plant = Plant.find_by(id: params[:id])
+  #   if confirm_destroy?
+  #     @plant.destroy
+  #     render json: { message: "Plant destroyed successfully" }
+  #   else
+  #     render json: { message: "Deletion canceled" }
+  #   end
+  # end
   
-  private
+  # private
   
-  def confirm_destroy?
-    confirm_message = "Are you sure you want to delete this plant?"
-    confirmation = params[:confirm]
-    confirmation == "true"
-  end
+  # def confirm_destroy?
+  #   confirm_message = "Are you sure you want to delete this plant?"
+  #   confirmation = params[:confirm]
+  #   confirmation == "true"
+  # end
   
   
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,13 +2,13 @@ class Schedule < ApplicationRecord
   belongs_to :user
   belongs_to :collected_plant
 
-  validates :watering_start_date, presence: true
-  validate :valid_watering_start_date
+  # validates :watering_start_date, presence: true
+  # validate :valid_watering_start_date
 
-  private
+  # # private
 
-  def valid_watering_start_date
-    errors.add(:watering_start_date, "must be a future date") if watering_start_date.present? && watering_start_date <= Date.today
-  end
+  # def valid_watering_start_date
+  #   errors.add(:watering_start_date, "must be a future date") if watering_start_date.present? && watering_start_date <= Date.today
+  # end
 end
 
